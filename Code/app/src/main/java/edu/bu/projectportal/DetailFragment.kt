5b8071b92d6235aa2098295e5f47ac67bb314ec9
maiweihao <whmai@bu.edu>
2023-07-19 11:20:22 +0800

    lab2

---
+++ Code/app/src/main/java/edu/bu/projectportal/DetailFragment.kt
@@ -0,0 +1,31 @@
+package edu.bu.projectportal
+
+import android.content.Context
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageButton
+import android.widget.TextView
+import android.widget.Toast
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+
+class DetailFragment : Fragment(R.layout.fragment_detail) {
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view,savedInstanceState)
+
+        val projTitle = view.findViewById<TextView>(R.id.projTitle)
+        val projDesc =  view.findViewById<TextView>(R.id.projDesc)
+        val editProj = view.findViewById<ImageButton>(R.id.editProj)
+
+        projTitle.text =  Project.project.title
+        projDesc.text = Project.project.description
+
+        editProj.setOnClickListener{
+            view.findNavController().
+            navigate(R.id.action_detailFragment_to_editFragment)
+        }
+    }
+}
---
+++ Code/app/src/main/java/edu/bu/projectportal/Project.kt
@@ -0,0 +1,9 @@
+package edu.bu.projectportal
+
+data class Project(val id: Int, var title: String, var description: String){
+    companion object {
+        val project = Project(0, "Find burger", "Authors of the project:weohao Mai\nProject links:https://github.com/CS683/project-weihaomai\nKeywords:Hamburger,burger shop\nDo you like it? ")
+    }
+
+
+}
